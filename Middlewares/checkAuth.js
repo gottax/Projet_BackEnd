83d1756e13345cdbdaddacc24cd5aec2d89c1522
gottax <96152857+gottax@users.noreply.github.com>
2023-01-10 22:21:09 +0100

    Update checkAuth.js

--- Middlewares/checkAuth.js
+++ Middlewares/checkAuth.js
@@ -1,1 +1,21 @@
+const express = require("express");
+const checkRequestFormat = require("./middlewares/checkRequestFormat");
+const errorHandler = require("./middlewares/errorHandler");
+const userRouter = require("./routes/users");
+const securityRouter = require("./routes/security");
+require("./models/db");
 
+const app = express();
+
+app.use(checkRequestFormat);
+app.use(express.json());
+
+app.use(securityRouter);
+app.use(userRouter);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server is listening on port " + PORT);
+});
